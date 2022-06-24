22e0bda12a0fa691db5b4ab5c44b0c9699cbf89c
Nguyễn Bảo Phước <baophuoc1903@gmail.com>
2022-06-24 10:03:58 +0700

    commit

--- predictive_coding.py
+++ predictive_coding.py
@@ -15,13 +15,12 @@ def decode(img_encode, coeff=None, order=None):
         order = coeff.shape[0]
 
     x = np.zeros((row, col+order))
-    print(x.shape, coeff.shape)
     for j in range(col):
         jj = j+order
         if j == 0:
-            x[:, jj] = img_encode[:, j] + np.around((coeff[:, order-1::-1] * x[:, (jj-1)::-1]).sum(), decimals=2)
+            x[:, jj] = img_encode[:, j] + (coeff[:, order-1::-1] * x[:, (jj-1)::-1]).sum(axis=1)
         else:
-            x[:, jj] = img_encode[:, j] + np.around((coeff[:, order-1::-1] * x[:, (jj-1):(jj-order-1):-1]).sum(), decimals=2)
+            x[:, jj] = img_encode[:, j] + (coeff[:, order-1::-1] * x[:, (jj-1):(jj-order-1):-1]).sum(axis=1)
 
     x = x[:, order:]
     return x
@@ -38,17 +37,19 @@ def encoder(img, coeff=None, order=None):
     if order is None:
         order = row
 
+    tmp_zeros = img.copy()
     for j in range(order):
-        tmp_zeros = np.zeros((row, col))
-        tmp_zeros[:, j:] = img[:, :row-j]
+        tmp_zeros[:, :j+1] = 0
+        tmp_zeros[:, j+1:] = img[:, :col-j-1]
         p = p + coeff[j] * tmp_zeros
 
+    # print('\n\n', p, '\n\n')
     return img - np.around(p)
 
 
 img = cv2.imread(r"images/Original.jpeg", 0)
-encode_img = encoder(img, order=100)
-decode_img = decode(encode_img.copy(), order=100)
+encode_img = encoder(img, order=None)
+decode_img = decode(encode_img.copy(), order=None)
 plt.figure(figsize=(10, 5))
 plt.subplot(131)
 plt.imshow(img, cmap='gray')
@@ -56,8 +57,7 @@ plt.subplot(132)
 plt.imshow(encode_img, cmap='gray')
 plt.subplot(133)
 plt.imshow(decode_img, cmap='gray')
-# print(encode_img, end='\n\n\n\n\n')
-# print(decode_img)
-cv2.imwrite('./images/Encoder_predictive_coding.jpeg', cv2.normalize(encode_img, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX))
 plt.show()
-
+cv2.imwrite('./images/Encoder_lossless_predictive_coding.jpeg', cv2.normalize(encode_img, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX))
+cv2.imwrite('./images/Decoder_lossless_predictive_coding.jpeg', decode_img.astype(int))
+print((img - decode_img.astype(int)).sum())
